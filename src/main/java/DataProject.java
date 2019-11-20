c5d38fba8e91a6ec19bda14f9439c3c507b88c8c
root <root@localhost.localdomain>
2019-11-20 14:40:09 +0530

    functionality

--- src/main/java/DataProject.java
+++ src/main/java/DataProject.java
@@ -3,9 +3,11 @@ import com.opencsv.CSVReaderBuilder;
 
 import java.io.FileReader;
 import java.util.List;
+import java.util.Scanner;
 
 class DataProject {
-    public static void main(String[] arg) {
+  static   void fun1()
+    {
         try {
             System.out.println("hey");
             String filename = "/root/Downloads/Compressed/matches.csv";
@@ -17,12 +19,57 @@ class DataProject {
                     System.out.print(cell + "\t");
                 }
 
-               System.out.println();
+                System.out.println();
             }
 
         } catch (Exception e) {
             System.out.println("File Read error" + " " + e.getMessage());
         }
+    }
+    static void fun2()
+    {
+
+    }
+    static void fun3()
+    {
+
+    }
+    static void fun4()
+    {
+
+    }
+    public static void main(String[] arg) {
+
+        System.out.println("IPL DATASET PROJECT");
+        System.out.println("Choose one of the Option Listed Below");
+        System.out.println("1. Number of matches played per year of all the years in IPL.");
+        System.out.println("2. Number of matches won of all teams over all the years of IPL.");
+        System.out.println("3. For the year 2016 get the extra runs conceded per team.");
+        System.out.println("4. For the year 2015 get the top economical bowlers.");
+        System.out.println("5. To exit");
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        switch (a)
+        {
+            case 1:
+                fun1();
+                break;
+            case 2:
+                fun2();
+                break;
+            case 3:
+                fun3();
+                break;
+            case 4:
+                fun4();
+                break;
+            case 5:
+                break;
+            default:
+                System.out.println("You Entered a wrong choice");
+        }
+
+
 
     }
 }
